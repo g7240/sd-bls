e924361b91754e2718927ed7a22bd6eb6f45a370
Jaromil <jaromil@dyne.org>
2024-05-18 04:56:13 +0200

    fixed sorting

--- benchmarks/bench.lua
+++ benchmarks/bench.lua
@@ -28,8 +28,13 @@ function test_many_issuance(num)
     local claim = OCTET.random(32)
     local rev = BIG.modrand(ECP.order())
     local sig = sign(A.sk, claim) + sign(rev, claim)
-    claims[claim] = { sig, G1 * rev, G2 * rev }
-    revocs[claim] = rev
+    table.insert(claims, {
+                   c = claim,
+                   s = sig,
+                   r1 = G1 * rev,
+                   r2 = G2 * rev
+    })
+    table.insert(revocs, { claim, rev })
   end
   return claims, revocs, os.clock() - start
 end
@@ -38,17 +43,17 @@ function test_many_proofs(num, creds)
   local start = os.clock()
   proofs = { }
   local c = 0
-  for k,v in pairs(creds) do
-    local sig = v[1] -- naked issuer's sig
-    local revG1 = v[2]
-    local revG2 = v[3]
+  for k,v in ipairs(creds) do
+    local sig = v.s -- naked issuer's sig
+    local revG1 = v.r1
+    local revG2 = v.r2
     local er = BIG.random()
     local tri = BIG.new(sha256:process(
                           (Miller(A.pk, revG1) ^ er):octet()
     ))
     table.insert(proofs, {
-                   id = m,
-                   s = sig + sign(tri, m),
+                   id = v.c,
+                   s = sig + sign(tri, v.c),
                    p = (revG2 + G2*tri):to_zcash(),
                    r = G1*er
     })
@@ -62,16 +67,18 @@ function test_many_verifs(num, proofs)
   local start = os.clock()
   local c = 0
 
-  for _,v in pairs(proofs) do
+  for k,v in ipairs(proofs) do
     local sig = v.s
     local pk = ECP2.from_zcash(v.p)
 --    assert( verify(pk + A.pk, v.id, sig) ) TODO
     verify(pk + A.pk, v.id, sig)
+    c = c + 1
     if c == num then break end
   end
   return os.clock() - start
 end
 
+TOTAL = TOTAL + STEP -- off by one fix
 print "issuance "
 local ISSUANCE_T = { }
 for i=10,TOTAL,STEP do
@@ -105,14 +112,14 @@ collectgarbage'collect'
 collectgarbage'collect'
 
 print''
-print("CLAIMS \t\t ISSUANCE \t\t PROOF")
+print("CLAIMS \t ISSUE \t PROVE \t VERIFY")
 for i=1,(TOTAL/STEP),1 do
   write(i*STEP)
-  write(' \t\t ')
+  write(' \t ')
   write(ISSUANCE_T[i])
-  write(' \t\t ')
+  write(' \t ')
   write(PROOF_T[i])
-  write(' \t\t ')
+  write(' \t ')
   write(VERIF_T[i])
   write('\n')
 end
