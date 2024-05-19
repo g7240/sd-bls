bda5008011a96c6cd337ac21a765513549361fed
Jaromil <jaromil@dyne.org>
2024-05-19 14:06:29 +0200

    revocations

--- benchmarks/bench.lua
+++ benchmarks/bench.lua
@@ -15,101 +15,46 @@
 -- License along with this program.  If not, see
 -- <https://www.gnu.org/licenses/>.
 
-A = keygen()
-sha256 = HASH.new('sha256')
+--A = keygen()
+--sha256 = HASH.new('sha256')
 TOTAL = 100
 STEP = 5
 
-function test_many_issuance(num)
-  local start = os.clock()
-  claims = { }
-  revocs = { }
-  for i=1,num do
-    local claim = OCTET.random(32)
-    local rev = BIG.modrand(ECP.order())
-    local sig = sign(A.sk, claim) + sign(rev, claim)
-    table.insert(claims, {
-                   c = claim,
-                   s = sig,
-                   r1 = G1 * rev,
-                   r2 = G2 * rev
-    })
-    table.insert(revocs, { claim, rev })
-  end
-  return claims, revocs, os.clock() - start
-end
-
-function test_many_proofs(num, creds)
-  local start = os.clock()
-  proofs = { }
-  local c = 0
-  for k,v in ipairs(creds) do
-    local sig = v.s -- naked issuer's sig
-    local revG1 = v.r1
-    local revG2 = v.r2
-    local er = BIG.random()
-    local tri = BIG.new(sha256:process(
-                          (Miller(A.pk, revG1) ^ er):octet()
-    ))
-    table.insert(proofs, {
-                   id = v.c,
-                   s = sig + sign(tri, v.c),
-                   p = (revG2 + G2*tri):to_zcash(),
-                   r = G1*er
-    })
-    c = c + 1
-    if c == num then break end
-  end
-  return proofs, os.clock() - start
-end
-
-function test_many_verifs(num, proofs)
-  local start = os.clock()
-  local c = 0
-  for k,v in ipairs(proofs) do
-    local sig = v.s
-    local pk = ECP2.from_zcash(v.p)
-    assert( verify(pk + A.pk, v.id, sig) )
-    c = c + 1
-    if c == num then break end
-  end
-  return os.clock() - start
-end
+-- some functions are in common.lua
 
 TOTAL = TOTAL + STEP -- off by one fix
-print "issuance "
+printerr "issuance "
 local ISSUANCE_T = { }
 for i=10,TOTAL,STEP do
-  write(i.." ")
+  printerr(i.." ")
   CLAIMS, REVOCS, T = test_many_issuance(i)
   table.insert(ISSUANCE_T, T)
 end
 collectgarbage'collect'
 collectgarbage'collect'
 
-print''
-print "proof "
+printerr '\n'
+printerr 'proof '
 local PROOF_T = { }
 for i=10,TOTAL,STEP do
-  write(i.." ")
+  printerr(i.." ")
   PROOFS, T = test_many_proofs(i, CLAIMS)
   table.insert(PROOF_T, T)
 end
 collectgarbage'collect'
 collectgarbage'collect'
 
-print''
-print "verification "
+printerr''
+printerr "verification "
 local VERIF_T = { }
 for i=10,TOTAL,STEP do
-  write(i.." ")
+  printerr(i.." ")
   T = test_many_verifs(i, PROOFS)
   table.insert(VERIF_T, T)
 end
 collectgarbage'collect'
 collectgarbage'collect'
 
-print''
 print("CLAIMS \t ISSUE \t PROVE \t VERIFY")
 for i=1,(TOTAL/STEP),1 do
   write(i*STEP)
--- benchmarks/common.lua
+++ benchmarks/common.lua
@@ -18,6 +18,8 @@
 CONF.output.encoding = { fun = get_encoding_function'url64',
                          name = 'url64' }
 
+A = keygen()
+sha256 = HASH.new('sha256')
 G1 = ECP.generator()
 G2 = ECP2.generator()
 HG1 = ECP.hashtopoint
@@ -42,4 +44,103 @@ function verify(pk, msg, sig)
 	  ==
     Miller(G2, sig)
    )
+end
+
+function test_many_issuance(num)
+  local start = os.clock()
+  claims = { }
+  revocs = { }
+  for i=1,num do
+    local claim = OCTET.random(32)
+    local rev = BIG.modrand(ECP.order())
+    local sig = sign(A.sk, claim) + sign(rev, claim)
+    table.insert(claims, {
+                   c = claim,
+                   s = sig,
+                   r1 = G1 * rev,
+                   r2 = G2 * rev
+    })
+    table.insert(revocs, { claim, rev })
+  end
+  return claims, revocs, os.clock() - start
+end
+
+function test_many_proofs(num, creds)
+  local start = os.clock()
+  proofs = { }
+  local c = 0
+  for k,v in ipairs(creds) do
+    local sig = v.s -- naked issuer's sig
+    local revG1 = v.r1
+    local revG2 = v.r2
+    local er = BIG.random()
+    local tri = BIG.new(sha256:process(
+                          (Miller(A.pk, revG1) ^ er):octet()
+    ))
+    table.insert(proofs, {
+                   id = v.c,
+                   s = sig + sign(tri, v.c),
+                   p = (revG2 + G2*tri):to_zcash(),
+                   r = G1*er
+    })
+    c = c + 1
+    if c == num then break end
+  end
+  return proofs, os.clock() - start
+end
+
+function test_many_verifs(num, proofs)
+  local start = os.clock()
+  local c = 0
+  for k,v in ipairs(proofs) do
+    local sig = v.s
+    local pk = ECP2.from_zcash(v.p)
+    assert( verify(pk + A.pk, v.id, sig) )
+    c = c + 1
+    if c == num then break end
+  end
+  return os.clock() - start
+end
+
+
+function revocation_contains(revocations, proof)
+  local res   = 0 -- store here result for constant time operations
+  for k,v in ipairs(revocations) do
+    if v[1] == proof.id then
+      local rev = v[2]
+      local tri =
+        BIG.new(
+          sha256:process(
+            (Miller(A.pk,proof.r)^rev)
+            :octet()
+        ))
+      if -- addendum of proof.p is equal to revG2
+        ECP2.from_zcash(proof.p) - (G2*tri) == G2*rev
+        and -- verify unblinded issuer signature
+        verify(A.pk, proof.id,
+               proof.s
+               - sign(tri, proof.id)
+               - sign(rev, proof.id))
+      then
+        res = 1
+      end
+    end
+  end
+  return res
+end
+
+function test_many_revocs(num, revocs, proofs)
+  local start = os.clock()
+  local c = 0
+  local found = 0
+  for k,v in ipairs(proofs) do
+    local sig = v.s
+    local pk = ECP2.from_zcash(v.p)
+    found = revocation_contains(revocs, v)
+    assert( verify(pk + A.pk, v.id, sig) )
+    c = c + 1
+    if c == num then break end
+  end
+  assert(found == 1)
+  return os.clock() - start
 end
